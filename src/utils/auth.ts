f6979d5d1e12748ada0d5fbdf0bd725747a9335b
denimozh <denimozh@gmail.com>
2024-02-25 00:00:30 +0000

    Added Authentication for navbar

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,10 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./connect";
 
-export const authOptions:NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID!,
