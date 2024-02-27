80f0803a12ed0e2223b585df878a7c7f0463d2ca
denimozh <denimozh@gmail.com>
2024-02-27 22:01:14 +0000

    Added Eslint and Prettier

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,155 +1,190 @@
-import React from 'react'
-import { FaArrowRightLong } from "react-icons/fa6";
-import styles from './style.module.css'
-import giftbook from "./img/giftbook.webp";
+import React from 'react';
+import { FaArrowRightLong } from 'react-icons/fa6';
+import styles from './style.module.css';
+import giftbook from './img/giftbook.webp';
 import Link from 'next/link';
 import { CatalogType } from '@/types/types';
-import Marquee from 'react-fast-marquee'
+import Marquee from 'react-fast-marquee';
 
 const getData = async () => {
-    const res = await fetch("http://localhost:3000/api/catalog",{
-        cache:"no-store"
-    })
+    const res = await fetch('http://localhost:3000/api/catalog', {
+        cache: 'no-store',
+    });
 
-    if(!res.ok){
-        throw new Error("Failed!");
+    if (!res.ok) {
+        throw new Error('Failed!');
     }
 
-    return res.json()
-}
+    return res.json();
+};
 
 const CatalogPage = async () => {
-
-  const catalog:CatalogType = await getData()
-  return (
-    <div className='h-[110vh] px-4'>
-        <div className='flex flex-row h-[55%] gap-1'>
-            <div className="w-[55%] bg-catalogBooks bg-auto rounded-[15px] bg-blend-soft-light bg-stone-700"  >
-                <div className='flex flex-col px-8 justify-between h-[100%] pb-10'>
-                    <div className='flex flex-row justify-between items-center ' >
-                        <div className='text-white pt-6 '>
-                            <p className='text-6xl'>New Arrivals</p>
-                        </div>
-                        <div className='pt-2'>
-                            <div className='z-1 cursor-pointer bg-black h-16 w-16 rounded-full flex items-center justify-center hover:scale-105 ease-in-out duration-500'>
-                                <FaArrowRightLong className='text-[35px] text-white'/>
+    const catalog: CatalogType = await getData();
+    return (
+        <div className="h-[110vh] px-4">
+            <div className="flex flex-row h-[55%] gap-1">
+                <div className="w-[55%] bg-catalogBooks bg-auto rounded-[15px] bg-blend-soft-light bg-stone-700">
+                    <div className="flex flex-col px-8 justify-between h-[100%] pb-10">
+                        <div className="flex flex-row justify-between items-center ">
+                            <div className="text-white pt-6 ">
+                                <p className="text-6xl">New Arrivals</p>
                             </div>
-                        </div>
-                    </div>
-                    <div className='text-white'>
-                        <p className='text-8xl'>+128</p>
-                        <div className='flex flex-col pt-3 gap-1'>
-                            <div className='flex flex-row gap-3'>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Fairy Tale by Stephen King</p>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Overkill by Sandra Brown</p>
-                            </div>
-                            <div className='flex flex-row gap-3'>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Crying in H Mart by Michael Zauner</p>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Ugly Love by Colleen Hoover</p>
-                            </div>
-                            <div className='flex flex-row gap-3'>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Babel by R.F Kaung</p>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Verify by Collen Hoover</p>
-                                <p className='font-medium bg-white p-4 text-black rounded-full'>Overkill by Sandra Brown</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Link href={"/catalog/bestsellers"} className='w-[45%] bg-white rounded-[15px] group cursor-pointer hover:bg-[#d8dbf8] ease-in-out duration-500'>
-                <div className='flex flex-col px-8 h-[55%] gap-1'>
-                    <div className='flex flex-row items-center justify-between '>
-                        <div className='text-black pt-6'>
-                            <p className='text-6xl'>Best Sellers</p>
-                        </div>
-                        <div className='pt-2'>
-                                <div className='z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-110 ease-in-out duration-500'>
-                                    <FaArrowRightLong className='text-[35px] text-white'/>
-                                </div>
-                        </div>
-                    </div>
-                        <div className='flex items-center justify-center h-full'>
-                            <div className='relative flex justify-center items-center align-center h-[400px] w-[500px] pt-32'>
-                                <div className={styles.blob}>      
+                            <div className="pt-2">
+                                <div className="z-1 cursor-pointer bg-black h-16 w-16 rounded-full flex items-center justify-center hover:scale-105 ease-in-out duration-500">
+                                    <FaArrowRightLong className="text-[35px] text-white" />
                                 </div>
                             </div>
                         </div>
-
-                </div>
-            </Link>
-        </div>
-        <div className='h-[65%] pt-1 grid grid-cols-[1fr_repeat(3,2fr)] grid-rows-[repeat(2,1fr)] gap-x-1 gap-y-1'>
-            <div className='cursor-default bg-[#FFE9D3] row-span-2 rounded-[25px] '>
-                <div className='flex flex-col '>
-                    <div className='w-[200px] -rotate-90'>
-                        <p className='text-8xl font-medium translate-y-4 -translate-x-[18rem] text-right'>All Genres</p>
+                        <div className="text-white">
+                            <p className="text-8xl">+128</p>
+                            <div className="flex flex-col pt-3 gap-1">
+                                <div className="flex flex-row gap-3">
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Fairy Tale by Stephen King
+                                    </p>
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Overkill by Sandra Brown
+                                    </p>
+                                </div>
+                                <div className="flex flex-row gap-3">
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Crying in H Mart by Michael Zauner
+                                    </p>
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Ugly Love by Colleen Hoover
+                                    </p>
+                                </div>
+                                <div className="flex flex-row gap-3">
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Babel by R.F Kaung
+                                    </p>
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Verify by Collen Hoover
+                                    </p>
+                                    <p className="font-medium bg-white p-4 text-black rounded-full">
+                                        Overkill by Sandra Brown
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {catalog.map((category) =>
-                <Link href={`/catalog/${category.slug}`} key={category.id} className={`group cursor-pointer ${category.color} ${category.hoverColor} rounded-[25px] ease-in-out duration-500`}>
-                    <div className=''>
-                        <div className='flex flex-row justify-between px-6'>
-                            <div className='pt-6 '>
-                                <p className='text-4xl font-medium'>{category.title}</p>
+                <Link
+                    href={'/catalog/bestsellers'}
+                    className="w-[45%] bg-white rounded-[15px] group cursor-pointer hover:bg-[#d8dbf8] ease-in-out duration-500"
+                >
+                    <div className="flex flex-col px-8 h-[55%] gap-1">
+                        <div className="flex flex-row items-center justify-between ">
+                            <div className="text-black pt-6">
+                                <p className="text-6xl">Best Sellers</p>
                             </div>
-                            <div className='pt-2'>
-                                        <div className='z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-105 ease-in-out duration-500'>
-                                            <FaArrowRightLong className='text-[35px] text-white'/>
-                                        </div>
+                            <div className="pt-2">
+                                <div className="z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-110 ease-in-out duration-500">
+                                    <FaArrowRightLong className="text-[35px] text-white" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center h-full">
+                            <div className="relative flex justify-center items-center align-center h-[400px] w-[500px] pt-32">
+                                <div className={styles.blob}></div>
                             </div>
                         </div>
                     </div>
                 </Link>
-            )}
-            
-        </div>
-        <div className='h-[55%] px-1 pt-1 pb-4'>
-            <div className='flex flex-row gap-1 h-[100%]'>
-                <Link href={"/catalog/usedbooks"} className='group cursor-pointer hover:bg-[#FB8484] flex flex-col basis-[43%] bg-[#F9A6A6] rounded-[25px] ease-in-out duration-500 h-[100%] px-6'>
-                    <div className='flex flex-row justify-between'>
-                        <div className='pt-6'>
-                            <p className='text-6xl font-medium'>Used Books</p>
+            </div>
+            <div className="h-[65%] pt-1 grid grid-cols-[1fr_repeat(3,2fr)] grid-rows-[repeat(2,1fr)] gap-x-1 gap-y-1">
+                <div className="cursor-default bg-[#FFE9D3] row-span-2 rounded-[25px] ">
+                    <div className="flex flex-col ">
+                        <div className="w-[200px] -rotate-90">
+                            <p className="text-8xl font-medium translate-y-4 -translate-x-[18rem] text-right">
+                                All Genres
+                            </p>
                         </div>
-                        <div className='pt-4'>
-                                    <div className='z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-110 ease-in-out duration-500'>
-                                        <FaArrowRightLong className='text-[35px] text-white'/>
+                    </div>
+                </div>
+                {catalog.map(category => (
+                    <Link
+                        href={`/catalog/${category.slug}`}
+                        key={category.id}
+                        className={`group cursor-pointer ${category.color} ${category.hoverColor} rounded-[25px] ease-in-out duration-500`}
+                    >
+                        <div className="">
+                            <div className="flex flex-row justify-between px-6">
+                                <div className="pt-6 ">
+                                    <p className="text-4xl font-medium">
+                                        {category.title}
+                                    </p>
+                                </div>
+                                <div className="pt-2">
+                                    <div className="z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-105 ease-in-out duration-500">
+                                        <FaArrowRightLong className="text-[35px] text-white" />
                                     </div>
-                        </div>
-                    </div>
-                    <div className='flex items-center justify-center h-full'>
-                            <div className='relative flex justify-center items-center align-center h-[400px] w-[500px] pt-5'>
-                                <div className={styles.blob1}>      
                                 </div>
                             </div>
-                    </div>
-                </Link>
-                <Link href={"/catalog/gift"} className='group relative cursor-pointer basis-[57%] bg-white hover:bg-[#F8F4D8] rounded-[25px] h-[100%] ease-in-out duration-500 flex flex-col justify-between gap-20'>
-                    <div className='flex flex-row justify-between px-6'>
-                        <div className='pt-6 '>
-                            <p className='text-6xl font-medium'>Gift Edition</p>
                         </div>
-                        <div className='pt-5'>
-                                    <div className='z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-105 ease-in-out duration-500'>
-                                        <FaArrowRightLong className='text-[35px] text-white'/>
-                                    </div>
+                    </Link>
+                ))}
+            </div>
+            <div className="h-[55%] px-1 pt-1 pb-4">
+                <div className="flex flex-row gap-1 h-[100%]">
+                    <Link
+                        href={'/catalog/usedbooks'}
+                        className="group cursor-pointer hover:bg-[#FB8484] flex flex-col basis-[43%] bg-[#F9A6A6] rounded-[25px] ease-in-out duration-500 h-[100%] px-6"
+                    >
+                        <div className="flex flex-row justify-between">
+                            <div className="pt-6">
+                                <p className="text-6xl font-medium">
+                                    Used Books
+                                </p>
+                            </div>
+                            <div className="pt-4">
+                                <div className="z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-110 ease-in-out duration-500">
+                                    <FaArrowRightLong className="text-[35px] text-white" />
+                                </div>
+                            </div>
                         </div>
+                        <div className="flex items-center justify-center h-full">
+                            <div className="relative flex justify-center items-center align-center h-[400px] w-[500px] pt-5">
+                                <div className={styles.blob1}></div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        href={'/catalog/gift'}
+                        className="group relative cursor-pointer basis-[57%] bg-white hover:bg-[#F8F4D8] rounded-[25px] h-[100%] ease-in-out duration-500 flex flex-col justify-between gap-20"
+                    >
+                        <div className="flex flex-row justify-between px-6">
+                            <div className="pt-6 ">
+                                <p className="text-6xl font-medium">
+                                    Gift Edition
+                                </p>
+                            </div>
+                            <div className="pt-5">
+                                <div className="z-1 bg-black cursor-pointer h-16 w-16 rounded-full flex items-center justify-center group-hover:scale-105 ease-in-out duration-500">
+                                    <FaArrowRightLong className="text-[35px] text-white" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="px-6">
+                            <img
+                                src={giftbook.src}
+                                className="w-[60%] h-[60%] rounded-[25px] group-hover:scale-95 ease-in-out duration-500"
+                            />
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            <div className="bg-white">
+                <Marquee autoFill={true} speed={85}>
+                    <div className="px-4">
+                        <p className="text-6xl text-black overflow-hidden py-10">
+                            Summer Sale -20%
+                        </p>
                     </div>
-                    <div className='px-6'>
-                        <img src={giftbook.src} className='w-[60%] h-[60%] rounded-[25px] group-hover:scale-95 ease-in-out duration-500'/>
-                    </div>
-                </Link>
+                </Marquee>
             </div>
         </div>
-        <div className='bg-white'>
-            <Marquee autoFill={true} speed={85} >
-                <div className='px-4'>
-                    <p className='text-6xl text-black overflow-hidden py-10'>Summer Sale -20%</p>
-                </div>
-            </Marquee>
-        </div>
-    </div>
-  )
-}
+    );
+};
 
-export default CatalogPage
+export default CatalogPage;
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,97 +1,122 @@
-"use client"
+'use client';
 
 import { OrderType } from '@/types/types';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { MdOutlineModeEdit } from "react-icons/md";
-import React from 'react'
+import { MdOutlineModeEdit } from 'react-icons/md';
+import React from 'react';
 import { toast } from 'react-toastify';
 
 const page = () => {
-  const {data:session, status} = useSession()
+    const { data: session, status } = useSession();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  if(status==="unauthenticated"){
-    router.push("/");
-  }
-
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn:({id, status}:{id:string, status:string}) => {
-      return fetch(`http://localhost:3000/api/orders/${id}`,{
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(status),
-      });
-    },
-    onSuccess(){
-      queryClient.invalidateQueries({queryKey:["orders"]});
+    if (status === 'unauthenticated') {
+        router.push('/');
     }
-  })
 
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['orders'],
-    queryFn: () => fetch('http://localhost:3000/api/orders').then((res) => res.json(),
-    )
-  });
+    const queryClient = useQueryClient();
 
-  const handleUpdate = (e:React.FormEvent<HTMLFormElement>, id:string) => {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const input = form.elements[0] as HTMLInputElement
-    const status = input.value;
+    const mutation = useMutation({
+        mutationFn: async ({ id, status }: { id: string; status: string }) => {
+            return await fetch(`http://localhost:3000/api/orders/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(status),
+            });
+        },
+        onSuccess() {
+            queryClient.invalidateQueries({ queryKey: ['orders'] });
+        },
+    });
 
-    mutation.mutate({id, status});
-    toast.success("The order status has been changed!");
-  }
+    const { isLoading, data } = useQuery({
+        queryKey: ['orders'],
+        queryFn: async () =>
+            await fetch('http://localhost:3000/api/orders').then(
+                async res => await res.json(),
+            ),
+    });
 
-  if (isLoading || status==="loading") return "Loading..."
-  return (
-    <div className='px-4 '>
-      <div className="p-4 px-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] bg-red-100 rounded-[20px]">
-        <div className='bg-red-300 p-4 rounded-[20px]'>
-          <table className="w-full border-separate border-spacing-3 bg-red-400 p-6 rounded-xl shadow-2xl">
-            <thead>
-              <tr className="text-left text-white">
-                <th className="hidden md:block text-white">Order ID</th>
-                <th className='text-white'>Date</th>
-                <th>Price</th>
-                <th className="hidden md:block">Products</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody className=''>
-              {data.map((item: OrderType)=>(
-                <tr className="text-sm md:text-base bg-red-200 " key={item.id}>
-                  <td className="hidden md:block py-6 px-1 font-bold">{item.id}</td>
-                  <td className="py-6 px-1 rounded-xl">{item.createdAt.toString().slice(0,10)}</td>
-                  <td className="py-6 px-1 rounded-xl">{item.price}</td>
-                  <td className="hidden md:block py-6 px-1 rounded-xl">{item.products[0].title}</td>
-                  {session?.user.isAdmin ? (
-                    <td>
-                      <form className='flex items-center justify-center gap-10 px-10' onSubmit={(e) => handleUpdate(e, item.id)}>
-                        <input placeholder={item.status} className='p-2 ring-1 ring-red-100 rounded-md w-[75%]'/>
-                        <button className='rounded-full bg-red-400 hover:bg-red-500 ease-in-out duration-500'>
-                            <MdOutlineModeEdit className='p-2 text-[50px]'/>
-                        </button>
-                      </form>
-                    </td>
-                  ) : (
-                    <td className="py-6 px-1 rounded-xl">{item.status}</td>
-                  )}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const input = form.elements[0] as HTMLInputElement;
+        const status = input.value;
+
+        mutation.mutate({ id, status });
+        toast.success('The order status has been changed!');
+    };
+
+    if (isLoading || status === 'loading') return 'Loading...';
+    return (
+        <div className="px-4 ">
+            <div className="p-4 px-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] bg-red-100 rounded-[20px]">
+                <div className="bg-red-300 p-4 rounded-[20px]">
+                    <table className="w-full border-separate border-spacing-3 bg-red-400 p-6 rounded-xl shadow-2xl">
+                        <thead>
+                            <tr className="text-left text-white">
+                                <th className="hidden md:block text-white">
+                                    Order ID
+                                </th>
+                                <th className="text-white">Date</th>
+                                <th>Price</th>
+                                <th className="hidden md:block">Products</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {data.map((item: OrderType) => (
+                                <tr
+                                    className="text-sm md:text-base bg-red-200 "
+                                    key={item.id}
+                                >
+                                    <td className="hidden md:block py-6 px-1 font-bold">
+                                        {item.id}
+                                    </td>
+                                    <td className="py-6 px-1 rounded-xl">
+                                        {item.createdAt.toString().slice(0, 10)}
+                                    </td>
+                                    <td className="py-6 px-1 rounded-xl">
+                                        {item.price}
+                                    </td>
+                                    <td className="hidden md:block py-6 px-1 rounded-xl">
+                                        {item.products[0].title}
+                                    </td>
+                                    {session?.user.isAdmin ? (
+                                        <td>
+                                            <form
+                                                className="flex items-center justify-center gap-10 px-10"
+                                                onSubmit={e => {
+                                                    handleUpdate(e, item.id);
+                                                }}
+                                            >
+                                                <input
+                                                    placeholder={item.status}
+                                                    className="p-2 ring-1 ring-red-100 rounded-md w-[75%]"
+                                                />
+                                                <button className="rounded-full bg-red-400 hover:bg-red-500 ease-in-out duration-500">
+                                                    <MdOutlineModeEdit className="p-2 text-[50px]" />
+                                                </button>
+                                            </form>
+                                        </td>
+                                    ) : (
+                                        <td className="py-6 px-1 rounded-xl">
+                                            {item.status}
+                                        </td>
+                                    )}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default page
+export default page;
